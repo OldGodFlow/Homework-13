70b770897689784615c43ee76d8544a241057afc
Old God Flow <writebogdanov@gmail.com>
2023-07-10 17:51:23 +0300

    hw

--- src/Author.java
+++ src/Author.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Author {
     private String firstName;
     private String lastName;
@@ -13,5 +15,31 @@ public class Author {
 
     public String getLastName() {
         return lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Автор книги: " +
+                firstName + ' ' +
+                lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Author)) return false;
+
+        Author author = (Author) o;
+
+        if (getFirstName() != null ? !getFirstName().equals(author.getFirstName()) : author.getFirstName() != null)
+            return false;
+        return getLastName() != null ? getLastName().equals(author.getLastName()) : author.getLastName() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getFirstName() != null ? getFirstName().hashCode() : 0;
+        result = 31 * result + (getLastName() != null ? getLastName().hashCode() : 0);
+        return result;
     }
 }
--- src/Book.java
+++ src/Book.java
@@ -24,4 +24,32 @@ public class Book {
     public void setYearPublished(int yearPublished) {
         this.yearPublished = yearPublished;
     }
+
+    @Override
+    public String toString() {
+        return "Название книги : " +
+                name + ", " +
+                author.toString() +
+                ", год публикации: " + yearPublished;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Book)) return false;
+
+        Book book = (Book) o;
+
+        if (getYearPublished() != book.getYearPublished()) return false;
+        if (getName() != null ? !getName().equals(book.getName()) : book.getName() != null) return false;
+        return getAuthor() != null ? getAuthor().equals(book.getAuthor()) : book.getAuthor() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getName() != null ? getName().hashCode() : 0;
+        result = 31 * result + (getAuthor() != null ? getAuthor().hashCode() : 0);
+        result = 31 * result + getYearPublished();
+        return result;
+    }
 }
